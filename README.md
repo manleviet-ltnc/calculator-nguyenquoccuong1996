dc21a3647076a6262849cbf94dd49de000aa2e97
nguyenquoccuong1996 <31847900+nguyenquoccuong1996@users.noreply.github.com>
2017-09-28 16:20:23 +0700

    caculator
    bước 1

--- README.md
+++
@@ -1,3 +0,0 @@
-# calculator-nguyenquoccuong1996
-calculator-nguyenquoccuong1996 created by GitHub Classroom
-máy tính bỏ túi
---
+++ caculator/caculator/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace caculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool isTypingnumber = false;
+
+        enum PhepToan { Cong, Tru, Nhan, Chia };
+        PhepToan pheptoan;
+
+        double nho;
+
+        private void NhapSo(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            NhapSo(btn.Text);
+        }
+
+        private void NhapSo(string so)
+        {
+            if (isTypingnumber)
+                lblDisplay.Text = lblDisplay.Text + so;
+            else
+            {
+                lblDisplay.Text = so;
+                isTypingnumber = true;
+            }
+
+        }
+
+        private void NhapPhepToan(object sender, EventArgs e)
+        {
+            TinhKetQua();
+
+            Button btn = (Button)sender;
+            switch (btn.Text)
+            {
+                case "+": pheptoan = PhepToan.Cong; break;
+                case "-": pheptoan = PhepToan.Tru; break;
+                case "*": pheptoan = PhepToan.Nhan; break;
+                case "/": pheptoan = PhepToan.Chia; break;
+            }
+
+            nho = double.Parse(lblDisplay.Text);
+
+            isTypingnumber = false;
+
+
+
+
+
+
+        }
+        private void TinhKetQua()
+        {
+            double tam = double.Parse(lblDisplay.Text);
+            double ketqua = 0.0;
+            switch (pheptoan)
+            {
+                case PhepToan.Cong: ketqua = nho + tam; break;
+                case PhepToan.Tru: ketqua = nho - tam; break;
+                case PhepToan.Nhan: ketqua = nho * tam; break;
+                case PhepToan.Chia: ketqua = nho / tam; break;
+            }
+            lblDisplay.Text = ketqua.ToString();
+        }
+
+        private void btnBang_Click(object sender, EventArgs e)
+        {
+            TinhKetQua();
+            isTypingnumber = false;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+    }
+}
+   
